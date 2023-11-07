f5a1c3635f2bd9382bdba35e76347ded2a87efbf
agustindiazad <agudiaaz1@gmail.com>
2023-11-07 17:58:11 -0300

    box

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
-import { Nav, NavbarContainer, NavLogo, Logo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtinLink, NavBtn } from '../../Components/Navbar/NavbarElements'
-import { Link, Element } from 'react-scroll';
-import Navbar from '../../Components/Navbar/index'
-import Sidebar from '../../Components/Sidebar/index'
-import Footer from '../../Components/Footer/index'
-import './Home.css'
+import React, { useState } from 'react';
+import { Link, Element, scroller } from 'react-scroll'; // Import Element and scroller from react-scroll
+import Navbar from '../../Components/Navbar/index';
+import Sidebar from '../../Components/Sidebar/index';
+import Footer from '../../Components/Footer/index';
+import './Home.css';
 
 function Home() {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
+
     const toggle = () => {
-        setIsOpen(!isOpen)
-    }
+        setIsOpen(!isOpen);
+    };
 
     return (
         <div className="Home">
@@ -19,15 +19,18 @@ function Home() {
                 <Navbar toggle={toggle} />
                 <div className='Pag-1'>
                     <h2 className='Sub-1'>Unete a nuestra comunidad y empieza a viajar y ganar dinero de una manera innovadora y útil</h2>
-                    <Link to="Pag-2" smooth={true} duration={500}>
-                        <button className='Bot-1'>¡Conocenos!</button>
+                    <Link to="Pag-2" smooth={true} duration={1000}>
+                        <button className='Bot-1'>¡Registrate aquí!</button>
                     </Link>
+                </div>
                 <div className='Pag-2'>
-                    <h2 className='Sub-2'>¿Quieres viajar?</h2>
+                    <div className='Box-1'>
+                        <h2 className='Sub-2'>Conductor</h2>
+                        <h2 className='Sub-3'>Pasajero</h2>
+                    </div>
                 </div>
             </div>
-        </div>
-        <Footer />
+            <Footer />
         </div>
     );
 }
